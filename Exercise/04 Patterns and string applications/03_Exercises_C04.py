62bae270ab90cd52bddb8b65b0c248db296961b5
jfernandoghe <30704196+jfernandoghe@users.noreply.github.com>
2017-10-22 20:17:26 -0500

    Update 03_Exercises_C04.py

--- Exercise/04 Patterns and string applications/03_Exercises_C04.py
+++ Exercise/04 Patterns and string applications/03_Exercises_C04.py
@@ -1,5 +1,15 @@
+#     Bioinformatics with Python 101
+#     José Fernando González Herrera
+#     jfernandoghe@gmail.com
+# The sequence contains a recognition site for the EcoRI restriction enzyme, which
+# cuts at the motif G*AATTC (the position of the cut is indicated by an asterisk).
+# Write a program which will calculate the size of the two fragments that will be
+# produced when the DNA sequence is digested with EcoRI.
 my_dna = "ACTGATCGATTACGTATAGTAGAATTCTATCATACATATATATCGATGCGTTCAT"
 frag1_length = my_dna.find("GAATTC") + 1
 frag2_length = len(my_dna) - frag1_length
 print("length of fragment one is " + str(frag1_length))
 print("length of fragment two is " + str(frag2_length))
+#     Credits to: Python for Biologist
+#                 Dr. Martin Jones
+                  http://www.pythonforbiologists.com/
