3fbdbe344e62f281934f696995e95bc0e93ec659
jfernandoghe <30704196+jfernandoghe@users.noreply.github.com>
2017-10-22 20:20:02 -0500

    Update 05_Exercises_C04.py

--- Exercise/04 Patterns and string applications/05_Exercises_C04.py
+++ Exercise/04 Patterns and string applications/05_Exercises_C04.py
@@ -1,14 +1,8 @@
 #     Bioinformatics with Python 101
 #     José Fernando González Herrera
 #     jfernandoghe@gmail.com
-# Here's a short section of genomic DNA:
-# ATCGATCGATCGATCGACTGACTAGTCATAGCTATGCATGTAGCTACTCGATCGATCGA
-# TCGATCGATCGATCGATCGATCGATCATGCTATCATCGATCGATATCGATGCATCGACT
-# ACTAT
-# It comprises two exons and an intron. The first exon runs from the start of the
-# sequence to the sixty-third character, and the second exon runs from the ninetyfirst
-# character to the end of the sequence. Write a program that will print just the
-# coding regions of the DNA sequence.
+# Using the data from part one, write a program that will calculate what percentage
+# of the DNA sequence is coding.
 my_dna ="ATCGATCGATCGATCGACTGACTAGTCATAGCTATGCATGTAGCTACTCGATCGATCGATCGATCGATCGATC
 GATCGATCGATCATGCTATCATCGATCGATATCGATGCATCGACTACTAT"
 exon1 = my_dna[0:62]
