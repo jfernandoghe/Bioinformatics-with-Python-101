c9fa5f93fd8072504fb2efd3bdb681696eb279e7
José Fernando González Herrera <30704196+jfernandoghe@users.noreply.github.com>
2017-10-30 00:43:52 -0600

    Create 03_Example_C04.py

---
+++ Exercise/04 Patterns and string applications/03_Example_C04.py
@@ -0,0 +1,17 @@
+class Human:
+    humanCo = 0
+    def __init__(self, name, DNA, hr, sp):
+    self.name = name                                #Name
+    self.DNA = DNA                                  #Small DNA sequence
+    self.hr = hd                                    #Heart disease
+    self.sp = db                                    #Diabetes
+    self.eyep = eyep                                #EyeProblems
+    self.k = k                                      #Kidney
+    Human.humanCo += 1
+def related(self, p1, p2):
+    self.p1 = p1.DNA
+
+
+sophia = Human("Sophia", "GCTA", "Y", "N", "Y", "N")
+susan = Human("Susan", "GCTA", "Y", "N", "Y", "N")
+related(sophia.d,susan)
